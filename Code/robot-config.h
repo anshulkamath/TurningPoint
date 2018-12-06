8569ce949b0d49107495a7035e558374c938a7bf
mindcoding <slimercraft3@gmail.com>
2018-12-06 18:20:03 -0500

    Update robot-config.h

--- Code/robot-config.h
+++ Code/robot-config.h
@@ -16,18 +16,18 @@ vex::limit Limit2(Brain.ThreeWirePort.Port[6]);
 vex::gyro GyroS(Brain.ThreeWirePort.F);
 vex::gyro GyroI(Brain.ThreeWirePort.E);
 /*
-vex::vision::signature GREENFLAG (1, -3117, -2419, -2768, -5135, -4253, -4694, 5.8, 0);
-vex::vision::signature REDFLAG (2, 8415, 8889, 8652, -911, -697, -804, 5.900000095367432, 0);
-vex::vision::signature BLUEFLAG (3, -3709, -2777, -3243, 8191, 11565, 9878, 4.5, 0);*/
-vex::controller Controller1;
-vex::vision::signature GREENFLAG (1, -2919, -2505, -2712, -5069, -4731, -4900, 11, 0);
-vex::vision::signature REDFLAG (2, 7281, 7735, 7508, -1115, -679, -896, 5, 0);
-vex::vision::signature BLUEFLAG (3, -3745, -3319, -3532, 10485, 11833, 11159, 7.3, 0);
-vex::vision::signature SIG_4 (4, 0, 0, 0, 0, 0, 0, 3, 0);
+vex::vision::signature GREENFLAG (1, -3475, -3251, -3362, -4955, -4645, -4800, 8, 0);
+vex::vision::signature REDFLAG (2, 8365, 8871, 8618, -713, -311, -512, 10.300000190734863, 0);
+vex::vision::signature BLUEFLAG (3, -3607, -3203, -3404, 11667, 12619, 12142, 6.900000095367432, 0);
+*/vex::controller Controller1;
+vex::vision::signature GREENFLAG (1, 0, 0, 0, 0, 0, 0, 10.5, 0);
+vex::vision::signature REDFLAG (2, 8365, 8871, 8618, -713, -311, -512, 10.300000190734863, 0);
+vex::vision::signature BLUEFLAG (3, -2521, -1431, -1976, 2615, 6087, 4350, 2, 0);
+vex::vision::signature SIG_4 (4, -1393, -1009, -1201, -2859, -2521, -2690, 2.4, 0);
 vex::vision::signature SIG_5 (5, 0, 0, 0, 0, 0, 0, 3, 0);
 vex::vision::signature SIG_6 (6, 0, 0, 0, 0, 0, 0, 3, 0);
 vex::vision::signature SIG_7 (7, 0, 0, 0, 0, 0, 0, 3, 0);
 //vex::vision::signature GREENFLAG (1, -1411, -153, -782, -6471, -5621, -6046, 5.5, 0);
 //vex::vision::signature REDFLAG (2, 1169, 9191, 5180, -1617, -621, -1120, 1.100000023841858, 0);
 
-vex::vision Vision (vex::PORT16, 61, GREENFLAG, REDFLAG, BLUEFLAG, SIG_4, SIG_5, SIG_6, SIG_7);
+vex::vision Vision (vex::PORT16, 74, GREENFLAG, REDFLAG, BLUEFLAG, SIG_4, SIG_5, SIG_6, SIG_7);
