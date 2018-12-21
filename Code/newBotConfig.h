c8d3113ebee42aab35de02eeda9304a7d54bfac6
mindcoding <slimercraft3@gmail.com>
2018-12-20 20:58:44 -0500

    Update newBotConfig.h

--- Code/newBotConfig.h
+++ Code/newBotConfig.h
@@ -1,9 +1,10 @@
 vex::brain Brain;
-vex::motor BackLeft = vex::motor(vex::PORT13, vex::gearSetting::ratio18_1);
+vex::motor BackLeft = vex::motor(vex::PORT20, vex::gearSetting::ratio18_1);
 vex::motor FrontLeft = vex::motor(vex::PORT11, vex::gearSetting::ratio18_1);
 vex::motor BackRight = vex::motor(vex::PORT12, vex::gearSetting::ratio18_1, true);
 vex::motor FrontRight = vex::motor(vex::PORT14, vex::gearSetting::ratio18_1, true);
 vex::motor Intake = vex::motor(vex::PORT15, vex::gearSetting::ratio36_1);
 vex::motor Shooter = vex::motor(vex::PORT16, vex::gearSetting::ratio6_1, true);
-
+vex::limit Limit1 = vex::limit(Brain.ThreeWirePort.A);
+vex::motor Flipper = vex::motor(vex::PORT10, vex::gearSetting::ratio18_1);
 vex::controller Controller1 = vex::controller();
