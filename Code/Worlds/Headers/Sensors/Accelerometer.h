ffb1cad2af28fdf2329c1bda259cc09da03ba0b8
mindcoding <slimercraft3@gmail.com>
2019-02-25 18:56:28 -0500

    Update Accelerometer.h

--- Code/Worlds/Headers/Sensors/Accelerometer.h
+++ Code/Worlds/Headers/Sensors/Accelerometer.h
@@ -1,4 +1,24 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class Accelerometer
 {
-
+    accelerometer accelX, accelY, accelZ;
+    vector<double> accelXData, accelYData, accelZData;
+  public:
+    
+    int accelFilterThread();
+    
+    Accelerometer(accelerometer accelX1, accelerometer accelY1, accelerometer accelZ1)
+    {
+      accelX = accelX1;
+      accelY = accelY1;
+      accelZ = accelZ1;
+    }
+    
+    void getXAccel();
+    void getYAccel();
+    void getZAccel();
 };
