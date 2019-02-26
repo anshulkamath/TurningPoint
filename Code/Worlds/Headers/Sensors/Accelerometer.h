fffdc2c3d4977fdc0c62d17f42be87007707bd68
Anshul Kamath <anshulkam@gmail.com>
2019-02-25 21:12:19 -0500

    Fix bugs via construction

--- Code/Worlds/Headers/Sensors/Accelerometer.h
+++ Code/Worlds/Headers/Sensors/Accelerometer.h
@@ -8,16 +8,12 @@ class Accelerometer
     accelerometer accelX, accelY, accelZ;
     vector<double> accelXData, accelYData, accelZData;
   public:
-    
+
     int accelFilterThread();
-    
-    Accelerometer(accelerometer accelX1, accelerometer accelY1, accelerometer accelZ1)
-    {
-      accelX = accelX1;
-      accelY = accelY1;
-      accelZ = accelZ1;
-    }
-    
+
+    Accelerometer(accelerometer accelX1, accelerometer accelY1, accelerometer accelZ1) :
+    accelX(accelX1), accelY(accelY1), accelZ(accelZ1) {}
+
     void getXAccel();
     void getYAccel();
     void getZAccel();
--- Code/Worlds/Headers/Sensors/Gyroscope.h
+++ Code/Worlds/Headers/Sensors/Gyroscope.h
@@ -1,9 +1,11 @@
+using namespace vex;
+
 class Gyroscope
 {
     gyro reg, inverse;
   public:
-    Gyroscope(gyro reg1, gyro inverse1) { reg = reg1; inverse = inverse1; }
-    
+    Gyroscope(gyro reg1, gyro inverse1) : reg(reg1), inverse(inverse1) {}
+
     double getAngle()
     {
       return (reg.value(analogUnits::range12bit) - reg.value(analogUnits::range12bit))/20;
