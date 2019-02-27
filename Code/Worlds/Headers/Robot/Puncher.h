bf46b8d6d8486a56e3f93662f20e908e4ec3b9f2
Jake Freeman <jake.freeman@volariscapital.com>
2019-02-26 19:55:08 -0500

    Puncher stuff

--- Code/Worlds/Headers/Robot/Puncher.h
+++ Code/Worlds/Headers/Robot/Puncher.h
@@ -1,18 +1,47 @@
+#include "robot-config.h"
+
+using namespace vex;
+using namespace std;
+
 class Puncher
 {
-  motor puncher;
-  motor angler;
-  Intake intake;
+  motor& puncher;
+  motor& angler;
+  limit& limter;
+  Intake& intake;
+  LineSensor& lineS;
 public:
-  Puncerh(motor p1, motor a1, Intake in) : puncher(p1), angler(a1), intake(in) {}
-  
-  void setAngle(double angle) 
+  Puncher(motor& p1, motor& a1, Intake& in, LineSensor& liney) :
+  puncher(p1), angler(a1), intake(in), lineS(liney) {}
+
+  void setAngle(double angle)
   {
-    
+
   }
-  
+
+  void firePuncher()
+  {
+    puncher.spin(direction::fwd);
+    while(limter.pressing());
+    while(!limter.pressing());
+    puncher.stop();
+
+  }
+  void waitForBall()
+  {
+    while(!lineS.isBall());
+  }
   void fire(double angle1, double angle2)
   {
-    
+    setAngle(angle1);
+    firePuncher();
+    intake.releaseBall();
+
+    setAngle(angle2);
+    waitForBall();
+    firePuncher();
+
   }
+
+
 };
--- Code/Worlds/Headers/Sensors/LineSensor.h
+++ Code/Worlds/Headers/Sensors/LineSensor.h
@@ -1,9 +1,10 @@
+
 class LineSensor
 {
   int threshHold = 0;
-  vex::line Line;
+  vex::line& Line;
  public:
-  LineSensor(vex::line Liney) : Line(Liney) {}
+  LineSensor(vex::line& Liney) : Line(Liney) {}
 
   bool isBall()
   {
