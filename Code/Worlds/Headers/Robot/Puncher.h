9ae3e91aea66ed10049d6ab678c6e2e52f1ba149
mindcoding <slimercraft3@gmail.com>
2019-02-26 19:01:38 -0500

    Update Puncher.h

--- Code/Worlds/Headers/Robot/Puncher.h
+++ Code/Worlds/Headers/Robot/Puncher.h
@@ -2,6 +2,17 @@ class Puncher
 {
   motor puncher;
   motor angler;
+  Intake intake;
 public:
+  Puncerh(motor p1, motor a1, Intake in) : puncher(p1), angler(a1), intake(in) {}
   
+  void setAngle(double angle) 
+  {
+    
+  }
+  
+  void fire(double angle1, double angle2)
+  {
+    
+  }
 };
