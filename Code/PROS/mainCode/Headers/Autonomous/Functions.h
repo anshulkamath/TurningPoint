89e5ce773d740f471447e53c7680426a2616ca1b
Anshul Kamath <anshulkam@gmail.com>
2019-02-27 18:30:20 -0500

    PROS Update

---
+++ Code/PROS/mainCode/Headers/Autonomous/Functions.h
@@ -0,0 +1,5 @@
+class Functions
+{
+  private:
+    Drivetrain& drive;
+};
---
+++ Code/PROS/mainCode/Headers/Sensors/Gyroscope.h
@@ -0,0 +1,13 @@
+using namespace vex;
+
+class Gyroscope
+{
+    gyro reg, inverse;
+  public:
+    Gyroscope(gyro reg1, gyro inverse1) : reg(reg1), inverse(inverse1) {}
+
+    double getAngle()
+    {
+      return (reg.value(analogUnits::range12bit) - reg.value(analogUnits::range12bit))/20;
+    }
+};
