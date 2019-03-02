360886b3d7f6bdc89fbffbeb3a9c7df6a221c9db
Jake Freeman <jake.freeman@volariscapital.com>
2019-03-01 21:17:56 -0500

    Source

--- Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/maincode@headers@robot@drivetrain.h
+++ Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/maincode@headers@robot@drivetrain.h
@@ -93,101 +93,19 @@ class Drivetrain
     Drivetrain (pros::Motor m1, pros::Motor m2, pros::Motor m3, pros::Motor m4, Gyroscope gyro1)
     : FrontRight(m1), FrontLeft(m2), BackRight(m3), BackLeft(m4), gyro(gyro1) {}
 
-    int driveTask()
-    {
-      pros::Controller cont (E_CONTROLLER_MASTER);
-      while(true)
-      {
-        int leftSide = cont.get_analog(ANALOG_LEFT_Y);
-        int rightSide = cont.get_analog(ANALOG_RIGHT_Y);
-        FrontLeft.move(leftSide);
-        FrontRight.move(rightSide);
-        BackRight.move(rightSide);
-        BackLeft.move(leftSide);
-        delay(10);
-      }
-    }
+    int driveTask();
 
-    void drive(double distance, double speed)
-    {
-      double target = distance/(4 * 3.1415) * 360;
+    void drive(double distance, double speed);
 
-      double kp = 0, P = 0;
-      double ki = 0, I = 0;
-      double kd = 0, D = 0;
-      double error = 0, lastError = 0;
-      double motorPower = 0;
-      double start = FrontRight.get_position();
+    void turnTo(double degrees, double speed);
 
-      double thresHold = 20;
-      while(true)
-      {
-        error = FrontRight.get_position() - (start);
-        P = kp * error;
+    void setDrive(int velocity);
 
-        if(error < 0)
-        {
-            I += ki * error;
-        }
+    void setDrive(int rightVel, int leftVel);
 
-        D = kd * (error - lastError);
+    void setBrakeMode(const motor_brake_mode_e mode);
 
-        motorPower = abs(P) + abs(I) - abs(D);
-        if(motorPower > abs(speed))
-          motorPower = abs(speed);
-        if(error < 0) motorPower *= -1;
-          setDrive(motorPower);
-      }
-      setDrive(0);
-    }
-
-    void turnTo(double degrees, double speed)
-    {
-      double error = degrees - gyro.getAngle();
-
-      if (abs(error) > 180)
-        error = sign(error) * (abs(error) - 180);
-
-      if (error < 0)
-        turnToLeft(error, speed);
-      else
-        turnToRight(error, speed);
-    }
-
-    void setDrive(int velocity)
-    {
-      FrontRight.move_velocity(velocity);
-      FrontLeft.move_velocity(velocity);
-      BackLeft.move_velocity(velocity);
-      BackRight.move_velocity(velocity);
-    }
-
-    void setDrive(int rightVel, int leftVel)
-    {
-      FrontRight.move_velocity(rightVel);
-      FrontLeft.move_velocity(leftVel);
-      BackLeft.move_velocity(leftVel);
-      BackRight.move_velocity(rightVel);
-    }
-
-    void setBrakeMode(const motor_brake_mode_e mode)
-    {
-        FrontRight.set_brake_mode(mode);
-        FrontLeft.set_brake_mode(mode);
-        BackLeft.set_brake_mode(mode);
-        BackRight.set_brake_mode(mode);
-    }
-
-    bool isDriving()
-    {
-        if (abs(FrontRight.get_actual_velocity()) < 5 &&
-            abs(BackRight.get_actual_velocity()) < 5 &&
-            abs(FrontLeft.get_actual_velocity()) < 5 &&
-            abs(BackLeft.get_actual_velocity()) < 5)
-            return false;
-        else
-          return true;
-    }
+    bool isDriving();
 
 
 };
