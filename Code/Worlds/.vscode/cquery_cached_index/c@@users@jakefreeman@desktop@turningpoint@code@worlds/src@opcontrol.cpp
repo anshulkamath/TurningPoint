67f95be3c762b182caad6a5728834cd64369b079
Jake Freeman <jake.freeman@volariscapital.com>
2019-03-03 23:31:11 -0500

    Fixes Cont

--- Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/src@opcontrol.cpp
+++ Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/src@opcontrol.cpp
@@ -2,25 +2,62 @@
 #include "../MainCode/headers/autonomous/AutonSelector.h"
 #include "../MainCode/headers/Sensors/Gyroscope.h"
 #include "../MainCode/headers/Robot/DriveTrain.h"
+#include "../MainCode/headers/Sensors/LineSensor.h"
 
-using namespace pros;
-
-void opcontrol()
-{
-	/*ADILineSensor line(1);
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	/*pros::ADILineSensor line(1);
 	line.calibrate();
-	lcd::print(1, "HHEE");
-	Task::delay(1000);
+	pros::lcd::print(1, "HHEE");
+	pros::Task::delay(1000);
 	while (true) {
-		lcd::clear_line(1);
-		lcd::print(1, "%d", line.get_value_calibrated());
+		pros::lcd::clear_line(1);
+		pros::lcd::print(1, "%d", line.get_value_calibrated());
 		//printf("%d", line.get_value());
-		delay(50);
+		pros::delay(50);
 	}*/
 		//displaySide();
+		//Drivetrain train(pros::Motor(1), pros::Motor(2), pros::Motor(3), pros::Motor(4), Gyroscope(pros::ADIGyro(5), pros::ADIGyro(6)));
+		//std::string x = "";
+		//pros::Task task(train.driveTask, &x);
+		pros::Motor FrontLeft(1), BackLeft(2), FrontRight(3), BackRight(4);
+		pros::Motor intake(5);
 
-		Drivetrain train(Motor(1), Motor(2), Motor(3), Motor(4), Gyroscope(ADIGyro(5), ADIGyro(6)));
-		std::string x = "Argument";
-		task_t driveTask = c::task_create(train.driveTask(), NULL, TASK_PRIORITY_MAX, TASK_STACK_DEPTH_DEFAULT, "Drive Task");
+		pros::Controller cont (E_CONTROLLER_MASTER);
+		while(true)
+		{
+			int leftSide = cont.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+			int rightSide = cont.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
+			FrontLeft.move(leftSide);
+			FrontRight.move(rightSide);
+			BackRight.move(rightSide);
+			BackLeft.move(leftSide);
+			delay(10);
 
+						if(cont.get_digital( E_CONTROLLER_DIGITAL_L1))
+			{
+				intake.move_velocity(100);
+			}else{
+
+				if(cont.get_digital(E_CONTROLLER_DIGITAL_L2))
+				{
+					intake.move_velocity(-100);
+				}else{
+					intake.move_velocity(0);
+				}
+			}
+			pros::delay(10);
+		}
 }
