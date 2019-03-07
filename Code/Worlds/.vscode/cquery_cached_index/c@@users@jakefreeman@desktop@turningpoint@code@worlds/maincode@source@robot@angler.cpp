a65894187efd2e73282d07c47b301d25373b8b31
Anshul Kamath <anshulkam@gmail.com>
2019-03-07 08:08:52 -0500

    Get rid of .vscode

--- Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/maincode@source@robot@angler.cpp
+++
@@ -1,1 +0,0 @@
-#include "../../Headers/Robot/Angler.h"
--- Code/Worlds/.vscode/cquery_cached_index/c@@users@jakefreeman@desktop@turningpoint@code@worlds/maincode@source@sensors@gyroscope.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../Headers/Sensors/Gyroscope.h"
-
-double Gyroscope::getAngle()
-{
-  return (reg.get_value() - inverse.get_value())/2;
-}
