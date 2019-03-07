918ec34a4ba2ee3ae19220b4a2a037731d6ff19a
mindcoding <slimercraft3@gmail.com>
2019-03-06 23:00:05 -0500

    Update temp.cpp

--- Code/Worlds/temp.cpp
+++ Code/Worlds/temp.cpp
@@ -1,23 +1,25 @@
 #include "robot-config.h"
 using namespace vex;
-int midFlag = 2260;
-int highFlag = 2450;
+int midFlag = 2403;
+int highFlag = 2656;
 int initPot;
 
 void setAngle(int potenVal)
 {
     Angler.setStopping(brakeType::hold);
-    double P = 0, kp = 0.1;
+    double P = 0, kp = 0.21, D = 0, kd = .1;
     double error = 100, lError = 100;
     double anglePower = 0;
 
-    while(fabs(error) > 10 && fabs(lError) > 10)
+    while(fabs(error) > 10 || fabs(lError) > 10)
     {
-        error = -potenVal + poten.value(analogUnits::range12bit);
+        error = -potenVal + Poten.value(analogUnits::range12bit);
         P = error * kp;
-
-        Angler.spin(directionType::fwd, P, velocityUnits::pct);
-        task::sleep(50);
+        //D = error - lError;
+        D *= kd;
+        lError = error;
+        Angler.spin(directionType::fwd, P + D, velocityUnits::pct);
+        task::sleep(10);
     }
 
     Angler.stop();
@@ -28,10 +30,12 @@ void setAngle(int potenVal)
 void doubleShot(int shot1, int shot2)
 {
     setAngle(shot1);
-    Puncher.rotateFor(1, rotationUnits::rev, 100, velocityUnits::pct);
-
-    task::sleep(1000);
-
+    int start = Puncher.rotation(rotationUnits::rev);
+    Puncher.rotateFor(1, rotationUnits::rev, 100, velocityUnits::pct, false);
+    //while(abs(Puncher.rotation(rotationUnits::rev)) - abs(start) < .24);
+    task::sleep(500);
+    Intake.spin(directionType::fwd, 100, velocityUnits::pct);
+    //while(Puncher.isSpinning());
     setAngle(shot2);
     Puncher.rotateFor(1, rotationUnits::rev, 100, velocityUnits::pct);
 }
@@ -41,7 +45,7 @@ int main()
     Angler.setStopping(brakeType::hold);
     while(true)
     {
-        Brain.Screen.printAt(30, 30, "%d", poten.value(analogUnits::range12bit));
+        Brain.Screen.printAt(30, 30, "%d", Poten.value(analogUnits::range12bit));
 
         if (Controller.ButtonX.pressing())
             Puncher.spin(directionType::fwd, 100, velocityUnits::pct);
@@ -55,7 +59,7 @@ int main()
         else
             Angler.stop();
         if(Controller.ButtonUp.pressing()) {
-            initPot = poten.value(analogUnits::range12bit);
+            initPot = Poten.value(analogUnits::range12bit);
             doubleShot(highFlag, midFlag);
         }
     }
